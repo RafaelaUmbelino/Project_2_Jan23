18bd31ca0f9ffd53c4d6de73a23bba0f55d06685
Rafaela Umbelino <rafaelaaumbelino@gmail.com>
2023-02-17 14:36:48 +0000

    initial commit

---
+++ public/js/script.js
@@ -0,0 +1,4 @@
+// https://developer.mozilla.org/en-US/docs/Web/API/Window/DOMContentLoaded_event
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Project_2_jan23 JS imported successfully!");
+});
