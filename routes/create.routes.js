bd28cb51da0a98694cde43b3f52fc2b0231693d9
Alexandre Alvaro <a.borges.alvaro@gmail.com>
2023-02-21 12:34:50 +0000

    added create favorite route

--- routes/create.routes.js
+++ routes/create.routes.js
@@ -18,11 +18,6 @@ router.post('/:id/wishlist',isLoggedIn, async (req, res, next) => {
 			wish_form_adr_address: adr_address,
 			wish_form_website: website,
 		} = req.body;
-/* 		console.log(_id);
-		console.log(place_id);
-		console.log(req.body);
-		console.log(name);
-		console.log(opening_hours); */
 
 		const restaurantSearched = await Restaurant.findOne({ place_id });
 		console.log(restaurantSearched)
@@ -52,27 +47,44 @@ router.post('/:id/wishlist',isLoggedIn, async (req, res, next) => {
 	}
 });
 
+
 router.post('/:id/favorites',isLoggedIn, async (req, res, next) => {
 	try {
 		const { _id } = req.session.currentUser;
 		const {
-			wish_form_place_id,
-			wish_form_name,
-			wish_form_opening_hours,
-			wish_form_photos,
-			wish_form_price_level,
-			wish_form_rating,
-			wish_form_url,
-			wish_form_adr_address,
-			wish_form_website,
+			favs_form_place_id: place_id,
+			favs_form_name: name,
+			favs_form_opening_hours: opening_hours,
+			favs_form_photos: photos,
+			favs_form_price_level: price_level,
+			favs_form_rating: rating,
+			favs_form_url: url,
+			favs_form_adr_address: adr_address,
+			favs_form_website: website,
 		} = req.body;
-		console.log(_id);
-		console.log(req.body);
-		console.log(wish_form_place_id);
-		console.log(wish_form_website);
-/* 		const restaurant = await Restaurant.findOne({ place_id: googleId });
-		if (restaurant) {
-		} */
+		
+		const restaurantSearched = await Restaurant.findOne({ place_id });
+		console.log(restaurantSearched)
+		if (restaurantSearched) {
+			await User.findByIdAndUpdate(_id, {$push: {favorites: restaurantSearched._id}})
+			console.log('restaurant found')
+		} else {
+			console.log("no restaurant found")
+			await Restaurant.create({ 
+				place_id, 
+				name,
+				opening_hours,
+				photos,
+				price_level,
+				rating,
+				url,
+				adr_address,
+				website,
+			});
+			const newRestaurant = await Restaurant.findOne({ place_id });
+			await User.findByIdAndUpdate(_id, {$push: {favorites: newRestaurant._id}})
+			console.log(newRestaurant);
+		}
 	} catch (error) {
 		console.log(error);
 		next(error);
